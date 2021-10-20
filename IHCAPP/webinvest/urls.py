4ae9c9040f267b38f4c27f811d2861b023bae28c
Rafael Oleques Nunes <oleques03@gmail.com>
2021-10-19 22:40:05 -0300

    Registros

--- IHCAPP/webinvest/urls.py
+++ IHCAPP/webinvest/urls.py
@@ -4,10 +4,10 @@ from . import record_views
 
 app_name = 'webinvest'
 urlpatterns = [
-    path('', record_views.show_records, name='record'),
+    path('', record_views.show_records, name='index'),
     path('record/', record_views.show_records, name='record'),
-    path('record/<int:record_id>/delete/', record_views.delete, name='delete'),
-    path('record/add/', record_views.add, name='add'),
-    path('record/<int:record_id>/edit/', record_views.edit, name='edit'),
+    path('record/<int:record_id>/delete/', record_views.delete, name='record_delete'),
+    path('record/add/', record_views.add, name='record_add'),
+    path('record/<int:record_id>/edit/', record_views.edit, name='record_edit'),
 
 ]
